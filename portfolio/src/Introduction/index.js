94717bcd03d6b1aac2a72c9d458f5c009ae0f3af
Rishabh Panwar <panwar.rishabh@gmail.com>
2019-10-02 02:35:56 +0530

    Update: - Added an background image. - Aligned name and other details for web. - Added CSS for the same.

--- portfolio/src/Introduction/index.js
+++ portfolio/src/Introduction/index.js
@@ -3,30 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookSquare, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 // import styled from 'styled-components';
 
-import { Background, Name, FName, LName, Description, Links } from './style';
+import { Background, Overlay, Name, FName, LName, Description, Links } from './style';
 
 class Introduction extends React.Component {
     render() {
         return (
             <Background>
-                <Name>
-                    <FName>Rishabh</FName>
-                    <LName>Panwar</LName>
-                </Name>
-                <Description>
-                    Webdeveloper and Programmer
-                </Description>
-                <Links>
-                    <a href="https://linkedin.com/in/rishabh-panwar-31511811a" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faLinkedin} />
-                    </a>
-                    <a href="https://github.com/RishabhPanwar" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faGithub} />
-                    </a>
-                    <a href="https://www.facebook.com/rishabh.panwar.35"  target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faFacebookSquare} />
-                    </a>
-                </Links>
+                <Overlay>
+                    <Name>
+                        <FName>Rishabh</FName>
+                        <LName>Panwar</LName>
+                        <Description>
+                        Webdeveloper and Programmer
+                    </Description>
+                    <Links>
+                        <a href="https://linkedin.com/in/rishabh-panwar-31511811a" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href="https://github.com/RishabhPanwar" target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href="https://www.facebook.com/rishabh.panwar.35"  target="_blank" rel="noopener noreferrer">
+                            <FontAwesomeIcon icon={faFacebookSquare} />
+                        </a>
+                    </Links>
+                    </Name>
+                </Overlay>
             </Background>
         );
     }   
--- portfolio/src/Introduction/style.js
+++ portfolio/src/Introduction/style.js
@@ -1,42 +1,71 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+// import backgroundImage from '../../public/images/background.jpg';
+const primaryColor = '#ffffff';
+const backgoundColor = '#444';
+const secondaryColor = '#eece1a';
+// const backgroundOpacity = '0.5';
+// const showHomeImage = 'true';
 
 const Background = styled.div`
     text-align: left;
-    padding-left: 100px;
-    background-color: #444;
+    padding: 4rem;
+    height: 100%
+    background-color: ${backgoundColor};
     height: 100vh;
+    font-family: Titillium Web;
+    letter-spacing: 2px;
+    background-image: url(images/background.jpg);
+    background-attachment: fixed;
+    background-size: cover;
+`;
+
+const Overlay = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(68,68,68,0.7);
 `;
 
 const Name = styled.div`
-    
+    position: fixed;
+    z-index: 2;
+    width: 100%;
+    margin: 20vh;
 `;
 
 const FName = styled.div`
     display: inline-block;
-    color: #FFFFFF;
+    color: ${primaryColor};
     font-size: 60px;
 `;
 
 const LName = styled.div`
     display: inline-block;
     margin-left: 5px;
-    color: yellow;
+    color: ${secondaryColor};
     font-size: 60px;
 `;
 
 const Description = styled.div`
-    color: #ffffff;
+    color: ${primaryColor};
     padding-left: 10px;
+    margin: 0px 0 5px 0;
 `;
 
 const Links = styled.div`
     > a {
         margin-left: 10px;
-        color: #ffffff;
+        color: ${primaryColor};
         > i {
             cursor: pointer;
+        }
+        :hover {
+            color: ${secondaryColor};
+            transition: all 0.5s ease-out; 
         }
     }
 `;
 
-export { Background, FName, LName, Name, Description,Links };
+export { Background, Overlay, FName, LName, Name, Description,Links };
